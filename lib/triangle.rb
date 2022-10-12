43d850cd2682ce8746fdce183107d68f9d03e2c5
thomasrcham <guseldorph@gmail.com>
2022-10-12 13:42:44 -0500

    not sure how to pass a test that asks for an error

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :leng1, :leng2, :leng3
+
+  def initialize(leng1, leng2, leng3)
+    @leng1 = leng1
+    @leng2 = leng2
+    @leng3 = leng3
+  end
+
+  def kind
+    if leng1 > 0 && leng2 > 0 && leng3 > 0
+      if leng1 + leng2 > leng3 || leng1 + leng3 > leng2 || leng2 + leng3 > leng1
+        if leng1 == leng2 && leng1 == leng3
+          :equilateral
+        elsif leng1 == leng2 || leng1 == leng3 || leng2 == leng3
+          :isosceles
+        else
+          :scalene
+        end
+      else
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+Triangle.new(0, 0, 0).kind
